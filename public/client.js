1c333666f904d61304b0db34fd05a9c52647c5d1
Dmitry Golikov <dmitry.golikov@devexpress.com>
2022-02-17 22:19:35 +0300

    badges

--- public/client.js
+++ public/client.js
@@ -8,6 +8,11 @@ console.log('hello world');
 window.TrelloPowerUp.initialize(
   {
     'card-badges': function(t, opts) {
-      return []
+      return t.card('all').then(function(card){
+        console.log(card);
+        return [{
+          text: card.idShort
+        }]
+      });
     }
   });
